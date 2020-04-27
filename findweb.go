2ac43092c156ae233a019a4a37037ae4cd3bc5dd
vmnguyen <manhnguyen510@gmail.com>
2020-04-27 17:27:16 +0700

    Add httpclient and read domains from file

--- findweb.go
+++ findweb.go
@@ -1,22 +1,71 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+
+	"github.com/valyala/fasthttp"
 )
 
-func main() {
-	f1 := flag.String("m", "domain", "Mode to scan: IP or Domain")
-	f2 := flag.String("f", "hosts.txt", "Input file to scan")
-	flag.Parse()
-	scanMode := *f1
-	scanFile := *f2
-	fmt.Println("scanMode:", scanMode)
-	fmt.Println("scanFile:", scanFile)
+var (
+	client *fasthttp.Client
+	ports  = []string{"80", "443", "8000", "8080"}
+)
+
+func doRequest(url string) {
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(resp)
+	req.SetRequestURI(url)
+	fasthttp.Do(req, resp)
+	bodyByte := resp.Body()
+	fmt.Println(string(bodyByte))
 }
 
+func scanPort(filePath string, mode bool) {
+	if mode {
+		// Scan by domain
+		fmt.Println("[~] Using list of domain")
+		file, _ := os.Open(filePath)
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+			doRequest(scanner.Text())
 
-func probe(target string, port int) bool{
-	// Probe probe here 
+		}
+
+	} else {
+		// Scan by IP address
+		fmt.Println("[~] Using list of IP address")
+	}
+}
+
+func main() {
+
+	var mode string
+	flag.StringVar(&mode, "m", "domain", "Input type: Domain or IP?")
+
+	var filePath string
+	flag.StringVar(&filePath, "f", "hosts.txt", "Filepath")
+
+	var concurrency int
+	flag.IntVar(&concurrency, "c", 50, "Concurrentcy")
+
+	flag.Parse()
+
+	if mode == "domain" {
+		scanPort(filePath, true)
+	} else {
+		scanPort(filePath, false)
+	}
+
+}
+
+func probe(target string, port int) bool {
+	// Probe probe here
 	return false
 }
